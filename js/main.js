a82cebc4a7e0413c3ef418f44fb3ab9b19f01a49
Patricia Martins <patricia_martins012@hotmail.com>
2023-09-10 11:48:15 -0400

    Adjusting the home page

--- js/main.js
+++ js/main.js
@@ -6,22 +6,26 @@ const testimonials = [
         author: "Emily Anderson, Executive Director of Helping Hands Foundation"
     },
     {
-        text: "Nec feugiat in fermentum posuere urna nec tincidunt. Vel fringilla est ullamcorper eget nulla. At quis risus sed vulputate odio ut enim blandit.",
-        author: "Ultrices neque ornare"
+        text: "“Working with students from the Interactive Media Design program at Fanshawe College was a game-changer for us. Their fresh ideas and innovative approach breathed new life into our project. Not only did they meet our expectations, but they exceeded them. We now have a cutting-edge website that has significantly boosted our online presence and customer engagement. We couldn’t be happier!”",
+        author: "Michael Davis, Creative Director at Spark Media Productions"
     },
     {
-        text: "Pulvinar elementum integer enim neque. Aliquet eget sit amet tellus cras adipiscing. Amet mauris commodo quis imperdiet massa tincidunt nunc. Donec ac odio tempor orci dapibus ultrices in iaculis nunc.",
-        author: "Enim tortor at auctor"
-    }
+        text: "“When we decided to embark on an ambitious interactive app project, we turned to Fanshawe College’s Interactive Media Design students for their exceptional talents. They delivered not only an app that met our objectives but one that received accolades from our users. The combination of their technical skills and creative thinking turned our vision into a reality. Working with them has been a delightful experience, and we hope to continue collaborating on new projects.”",
+        author: "Victoria Stratton, CEO of InnovateTech"
+    },
+    {
+        text: "“We had a complex project that demanded cutting-edge interactive design skills. After collaborating with a graduate from this program, we were thoroughly impressed. Their dedication, attention to detail, and commitment to delivering high-quality work were truly remarkable. Thanks to their efforts, our project was a resounding success, and we look forward to future collaborations.”",
+        author: "Harrison Thornton, Marketing Manager at Acme Creative Studios"
+    },
 ];
 
 let currentIndex = 0;
 
 const testimonialText = document.querySelector(".testimonial-text"),
-testimonialAuthor = document.querySelector(".testimonial-author"),
-prevButton = document.getElementById("prevBtn"),
-nextButton = document.getElementById("nextBtn"),
-counterSquares = document.querySelectorAll(".counter-square");
+    testimonialAuthor = document.querySelector(".testimonial-author"),
+    prevButton = document.getElementById("prevBtn"),
+    nextButton = document.getElementById("nextBtn"),
+    counterSquares = document.querySelectorAll(".counter-square");
 
 function displayTestimonial(index) {
     const testimonial = testimonials[index];
@@ -51,10 +55,10 @@ nextButton.addEventListener("click", () => {
 // card slider
 
 const wrapper = document.querySelector(".wrapper"),
-carousel = document.querySelector(".carousel"),
-firstCardWidth = carousel.querySelector(".card-selected").offsetWidth,
-arrowBtns = document.querySelectorAll(".wrapper .icon"),
-carouselChildrens = [...carousel.children];
+    carousel = document.querySelector(".carousel"),
+    firstCardWidth = carousel.querySelector(".card-selected").offsetWidth,
+    arrowBtns = document.querySelectorAll(".wrapper .icon"),
+    carouselChildrens = [...carousel.children];
 
 let isDragging = false, startX, startScrollLeft, timeoutId;
 
@@ -92,7 +96,7 @@ const dragStart = (e) => {
 }
 
 const dragging = (e) => {
-    if(!isDragging) return; // if isDragging is false return from here
+    if (!isDragging) return; // if isDragging is false return from here
     // Updates the scroll position of the carousel based on the cursor movement
     carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
 }
@@ -104,13 +108,13 @@ const dragStop = () => {
 
 const infiniteScroll = () => {
     // If the carousel is at the beginning, scroll to the end
-    if(carousel.scrollLeft === 0) {
+    if (carousel.scrollLeft === 0) {
         carousel.classList.add("no-transition");
         carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
         carousel.classList.remove("no-transition");
     }
     // If the carousel is at the end, scroll to the beginning
-    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+    else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
         carousel.classList.add("no-transition");
         carousel.scrollLeft = carousel.offsetWidth;
         carousel.classList.remove("no-transition");
@@ -126,7 +130,7 @@ wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
 
 // video
 
-src="https://player.vimeo.com/api/player.js"
+src = "https://player.vimeo.com/api/player.js"
 
 
 
